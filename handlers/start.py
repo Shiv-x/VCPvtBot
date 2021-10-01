6b62016e4b9932b06070fea2dab606ad960fe9e9
QueenArzoo <64604049+QueenArzoo@users.noreply.github.com>
2021-10-01 14:57:47 +0800

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -12,7 +12,7 @@ async def start(_, message: Message):
         text="**Hello 👋🏻 {}!**\n\nI **Can Play Music In Voice Chats of Telegram Groups.**I Have A **lot of cool feature that will amaze You!**\n\n**Click /help For More Help On My Usage ❤**".format(message.from_user.mention),
         reply_markup=InlineKeyboardMarkup(
             [[
-            InlineKeyboardButton("➕ Add To Your Group ➕", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
+            InlineKeyboardButton("➕ Add To Your Group ➕", url=f"https://t.me/ALMusicBot?startgroup=true")
             ],[
             InlineKeyboardButton("💬 Group", url="https://t.me/AwesomeSupport"),
             InlineKeyboardButton("Channel", url="https://t.me/LaylaList")
